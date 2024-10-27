0474bd72f0faa6f9e38907d0daf9bcea5af3383f
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2024-10-27 10:32:37 +0300

    Создал модель Category, прописал ее в админке. Провел миграции.

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from mptt.admin import DraggableMPTTAdmin
 
-from .models import Post
+from .models import Post, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(DraggableMPTTAdmin):
+    """
+    Админ панель модели категорий
+    """
+
+    prepopulated_fields = {"slug": ("title",)}
+
 
 admin.site.register(Post)

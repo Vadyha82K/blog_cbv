90ecfbffe1501956fb5447b834ebc112e4fd2073
Konovalov Vadim <Konovalov-Vadim2008@yandex.ru>
2024-11-06 17:10:46 +0300

    Добавил модель и форму для создания комментария

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -1,21 +1,17 @@
 from django import forms
 
-from .models import Post
+from .models import Post, Comment
 
 
 class PostCreateForm(forms.ModelForm):
-    """
-    Форма добавления статей на сайте
-    """
+    """Форма добавления статей на сайте"""
 
     class Meta:
         model = Post
         fields = ("title", "category", "description", "text", "thumbnail", "status")
 
     def __init__(self, *args, **kwargs):
-        """
-        Обновление стилей формы под Bootstrap
-        """
+        """Обновление стилей формы под Bootstrap"""
         super().__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs.update(
@@ -24,18 +20,35 @@ class PostCreateForm(forms.ModelForm):
 
 
 class PostUpdateForm(PostCreateForm):
-    """
-    Форма обновления статьи на сайте
-    """
+    """Форма обновления статьи на сайте"""
 
     class Meta:
         model = Post
         fields = PostCreateForm.Meta.fields + ("fixed",)
 
     def __init__(self, *args, **kwargs):
-        """
-        Обновление стилей формы под Bootstrap
-        """
+        """Обновление стилей формы под Bootstrap"""
         super().__init__(*args, **kwargs)
 
         self.fields["fixed"].widget.attrs.update({"class": "form-check-input"})
+
+
+class CommentCreateForm(forms.ModelForm):
+    """Форма добавления комментариев к статьям"""
+
+    parent = forms.IntegerField(widget=forms.HiddenInput, required=False)
+    content = forms.CharField(
+        label="",
+        widget=forms.Textarea(
+            attrs={
+                "cols": 30,
+                "rows": 5,
+                "placeholder": "Комментарий",
+                "class": "form-control",
+            }
+        ),
+    )
+
+    class Meta:
+        model = Comment
+        fields = ("content",)
